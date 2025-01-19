101f5890f20347718eecdcd5c791daec9b5ea2ba
DannyNavi <nguyeda3@oregonstate.edu>
2025-01-19 02:33:06 -0800

    Updated login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,2 +1,35 @@
 <a href="/">back to home</a>
-<h1>login page</h1>
+<h1>Login</h1>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        margin: auto;
+        border: 1px solid;
+        border-radius: 25px; 
+        width: 350px;
+        height: 150px;
+    }
+    h1 {
+        text-align: center;
+    }
+    input {
+        border-radius: 25px;
+        text-align: center;
+        height: 30px;
+        margin: 2px;
+    }
+    button {
+        justify-content: right;
+        margin: 5px;
+    }
+</style>
+
+<div>
+    <input type="text" placeholder="Username" >
+    <input type="text" placeholder="Password" >
+    <button>Submit</button>
+</div>
