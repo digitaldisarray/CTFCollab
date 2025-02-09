05c04d8f2fb7583213ee9da115a62ae5328079e9
DannyNavi <nguyeda3@oregonstate.edu>
2025-02-08 16:36:03 -0800

    added a home button to event page

---
+++ src/routes/pages/event-room/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+</script>
+
+<style>
+    .CTF {
+        color: #dc4405;
+    }
+    h1 {
+        font-size: 2.5rem;
+        margin-left: 0px;
+        padding-left: 0px;
+    }
+    .logo {
+        margin-left: 0px;
+        padding-left: 0px;
+    }
+</style>
+
+<div class="logo">
+    <Button
+        href="/"
+        variant="ghost"
+    >
+    <h1><span class="CTF">CTF</span><span class="Collab">Collab</span></h1>
+    </Button> 
+</div>
