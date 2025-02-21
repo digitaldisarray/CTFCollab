9c0026cdf150e9f8c6df138e108512bde1a3cb36
digitaldisarray <digitaldisarray@users.noreply.github.com>
2025-02-21 15:58:37 -0800

    Automatic admin account creation

--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -1,10 +1,13 @@
 package handler
 
 import (
+	"context"
 	"database/sql"
 	"log"
+	"os"
 	"time"
 
+	"github.com/alexedwards/argon2id"
 	db "github.com/digitaldisarray/ctfcollab/db/sqlc"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/golang-migrate/migrate/v4"
@@ -56,5 +59,43 @@ func LoadHandler(dbUrl string) (*Handler, error) {
 	queries := db.New(handle)
 	handler := NewHandler(queries)
 
+	// Create an admin account if specified and we haven't already
+	CreateAdmin(queries)
+
 	return handler, nil
+}
+
+func CreateAdmin(queries *db.Queries) {
+	admin_username, found := os.LookupEnv("ADMIN_USERNAME")
+	if !found || admin_username == "" {
+		log.Println("ADMIN_USERNAME environment variable not set, skipping creation")
+		return
+	}
+
+	admin_password, found := os.LookupEnv("ADMIN_PASSWORD")
+	if !found || admin_password == "" {
+		log.Println("ADMIN_PASSWORD environment variable not set, skipping creation")
+		return
+	}
+
+	// Make sure admin account doesn't already exist
+	ctx := context.Background()
+	_, err := queries.GetUserByUsername(ctx, admin_username)
+	if err == nil {
+		log.Printf("ADMIN_USERNAME: %s already exists, skipping creation\n", admin_username)
+		return
+	}
+
+	// Hash admin password
+	hash, err := argon2id.CreateHash(admin_password, argon2id.DefaultParams)
+	if err != nil {
+		log.Printf("Error hashing admin password: %v", err)
+		return
+	}
+
+	// Create the admin account
+	params := new(db.CreateAdminParams)
+	params.Username = admin_username
+	params.PasswordHash = hash
+	queries.CreateAdmin(ctx, *params)
 }
