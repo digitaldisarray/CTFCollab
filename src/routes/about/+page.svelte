38e6c9a1246105347fe05945c1cacda40fa4ed42
SOry Diagouraga <diagours@oregonstate.edu>
2025-03-02 22:19:32 -0800

    adding about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+
+<script>
+    import JoinRoom from "../../components/JoinRoom.svelte";
+</script>
+<style>
+    a {
+        float: right;
+    }
+</style>
+<h1>CTFCollab</h1>
+
+<a href='/login'>login</a>
+<JoinRoom/>
+
