f0238fac0bb856334c3941b8bfb45c8643e0ff36
brayden-aldrich <aldricbr@oregonstate.edu>
2025-04-23 13:37:00 -0700

    Changed the request to a client side jwt decode function.
    The original bug was arising due to communication between the server side render space and the docker container. Potential fixes included replacing localhost:1337 to backend:1337 or using 127.0.0.1, yet these did not work. To avoid further headache I just decided to decode it without worrying about backend connection from a SSR.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,27 +1,25 @@
 import type { Handle } from '@sveltejs/kit'
 
+// parse the jwt token for username and admin bool
+const parseJwt = (token: String) => {
+    try {
+        return JSON.parse(atob(token.split('.')[1]));
+    } catch (e) {
+        return null;
+    }
+};
+
 export const handle: Handle = async ({ event, resolve }) => {
     const jwt = event.cookies.get('token')
    
     if(jwt){
-        let response = await fetch("http://localhost:1337/users/token", {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                Cookie: `token=${jwt}` // hooks.server.ts is beyond the context of the browser, so need to specify the cookie here
-            },
-        });
-  
-        if(response.ok){
-            let resp = await response.json();
+        let resp = parseJwt(jwt)
+        
+        if(resp != null){
             event.locals.user = {
-                username: resp.username,
+                username: resp.name,
                 isAdmin: resp.isAdmin
             };
-            
-
-        } else {
-            event.locals.user = null;
         }
         
     } else {
